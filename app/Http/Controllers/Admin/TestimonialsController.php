f0320fd6aa613546785ef992d8b61bafcf47d602
DenisBashkirov <era-digital@yandex.ru>
2018-12-13 19:56:48 +0300

    commit

--- app/Http/Controllers/Admin/TestimonialsController.php
+++ app/Http/Controllers/Admin/TestimonialsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 
-class TestimonialsContrller extends Controller
+class TestimonialsController extends Controller
 {
     /**
      * Display a listing of the resource.
