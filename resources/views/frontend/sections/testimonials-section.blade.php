075eed0de6f9956dd6564e285672dc1c4e7c1fab
DenisBashkirov <era-digital@yandex.ru>
2018-12-13 18:40:45 +0300

    Update testimonials-section.blade.php

--- resources/views/frontend/sections/testimonials-section.blade.php
+++ resources/views/frontend/sections/testimonials-section.blade.php
@@ -1,18 +1,20 @@
-<section class="testimonials-section section container">
+@if(count($testimonials) > 0)
+    <section class="testimonials-section section container">
 
-    <h2 class="h2 testimonials-section__header section__header">Отзывы</h2>
+        <h2 class="h2 testimonials-section__header section__header">Отзывы</h2>
 
-    <div class="testimonials-section__body section__body">
+        <div class="testimonials-section__body section__body">
 
-        @foreach($testimonials as $item)
-            @component('frontend.components.testimonial-card', ['testimonial'=>$item])
-            @endcomponent
-        @endforeach
+            @foreach($testimonials as $item)
+                @component('frontend.components.testimonial-card', ['testimonial'=>$item])
+                @endcomponent
+            @endforeach
 
-    </div>
+        </div>
 
-    <div class="testimonials-section__btns section__btns">
-        <a href="#" class="btn btn_size_md btn_color_green">Все отзывы</a>
-    </div>
+        <div class="testimonials-section__btns section__btns">
+            <a href="#" class="btn btn_size_md btn_color_green">Все отзывы</a>
+        </div>
 
-</section>
+    </section>
+@endif
