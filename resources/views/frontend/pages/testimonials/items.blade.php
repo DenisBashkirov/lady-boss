35b3dd0e211e14d2360b5165aeb8f8238dfe929a
DenisBashkirov <era-digital@yandex.ru>
2018-12-14 12:29:29 +0300

    commit

--- resources/views/frontend/pages/testimonials/items.blade.php
+++ resources/views/frontend/pages/testimonials/items.blade.php
@@ -6,5 +6,12 @@
             @endcomponent
         @endforeach
     </div>
+
+    <div class="section__btns">
+        <div class="btn btn_size_md btn_color_white js-action_elem" data-target="testimonial-add" data-effect="slideDown">Написать отзыв</div>
+    </div>
+
+    @component('frontend.components.forms.testimonial_add')
+    @endcomponent
     
 </div>
